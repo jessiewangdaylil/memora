0cdbf397780f09d12186752e51c03dabefe1bf64
wanting <wantingpobo@gamil.com>
2023-06-06 16:01:27 +0800

    index:banner立可拍+五線譜滑入 / blog:留言便利貼

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,9 +22,16 @@
     <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
    <link rel="stylesheet" href="{{asset('fontawesome-free-6.2.1-web\css\all.min.css')}}">
+
+
+
    @yield('css')
    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.1/dist/cdn.min.js"></script>
    <script src="https://kit.fontawesome.com/2492741ca4.js" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></script>
+
+
+
    @livewireStyles
 
 </head>
@@ -113,7 +120,7 @@
                                 </li>
 
                                 <li>
-                                  <a href="{{url('/register')}}"><img class="register-icon" src="{{asset('img/regester-128.png')}}" alt=""></a>
+                                  <a href="{{url('/register')}}"><img class="register-icon" src="{{asset('img/regester-128.png')}}" alt=""></span></a>
                                 </li>
                             </ul>
                         </div>
@@ -315,6 +322,95 @@ slideBg.addEventListener('mousemove', function(e) {
 //     logoBird.style.top = mouseY + 'px';
 // });
 
+
+// -----------------------------------------------
+
+// 音樂盒圖片縮放
+gsap.fromTo('.choice-watch-img', {
+  scale: 1,
+}, {
+  scale: 1.1,
+  repeat: -1,
+  duration: 6,
+  ease: 'power3.inOut',
+  yoyo: true
+});
+
+// 照片回憶區
+const pictures = document.querySelectorAll('.Photo-Memory-Picture');
+var previousTouch = undefined;
+
+function updateElementPosition(element, event) {
+  var movementX, movementY;
+
+  if (event.type === 'touchmove') {
+    const touch = event.touches[0];
+    movementX = previousTouch ? touch.clientX - previousTouch.clientX : 0;
+    movementY = previousTouch ? touch.clientY - previousTouch.clientY : 0;
+    previousTouch = touch;
+  } else {
+    movementX = event.movementX;
+    movementY = event.movementY;
+  }
+
+  const elementY = parseInt(element.style.top || 0) + movementY;
+  const elementX = parseInt(element.style.left|| 0) + movementX;
+
+  element.style.top = elementY + "px";
+  element.style.left = elementX + "px";
+}
+
+function startDrag(element, event) {
+  const updateFunction = (event) => updateElementPosition(element, event);
+  const stopFunction = () => stopDrag({update: updateFunction, stop: stopFunction});
+  document.addEventListener("mousemove", updateFunction);
+  document.addEventListener("touchmove", updateFunction);
+  document.addEventListener("mouseup", stopFunction);
+  document.addEventListener("touchend", stopFunction);
+}
+
+function stopDrag(functions) {
+  previousTouch = undefined;
+  document.removeEventListener("mousemove", functions.update);
+  document.removeEventListener("touchmove", functions.update);
+  document.removeEventListener("mouseup", functions.stop);
+  document.removeEventListener("touchend", functions.stop);
+}
+
+pictures.forEach(picture => {
+  const range = 100;
+  const randomX = Math.random() * (range * 2) - range;
+  const randomY = Math.random() * (range * 2) - range;
+  const randomRotate = Math.random() * (range / 2) - range / 4;
+  const startFunction = (event) => startDrag(picture, event);
+  picture.style.top = `${randomY}px`;
+  picture.style.left = `${randomX}px`;
+  picture.style.transform = `translate(-50%, -50%) rotate(${randomRotate}deg)`;
+  picture.addEventListener("mousedown", startFunction);
+  picture.addEventListener("touchstart", startFunction);
+});
+
+
+//五線譜動態
+// 获取目标元素和动画元素
+const introductionSection = document.querySelector('.Introduction');
+const curveAnimationElement = document.querySelector('.curve-animation img');
+
+// 监听滚动事件
+window.addEventListener('scroll', function() {
+  // 获取目标元素的位置信息
+  const rect = introductionSection.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  // 判断目标元素是否进入视窗
+  if (rect.top <= windowHeight) {
+    // 添加动画类
+    curveAnimationElement.classList.add('animate');
+  } else {
+    // 移除动画类（可选）
+    curveAnimationElement.classList.remove('animate');
+  }
+});
 
 
 
