bc144a74c3121cf76d63d0ceebdcaa683c8cb571
Jessie Wang <jessie.wang.daylil@gmail.com>
2023-06-06 21:25:23 +0800

    啟用會員功能(未完成)

---
+++ app/View/Livewire/EnableMember.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Livewire;
+
+use Livewire\Component;
+
+class EnableMember extends Component
+{
+    public function render()
+    {
+        return view('livewire.enable-member');
+    }
+}
